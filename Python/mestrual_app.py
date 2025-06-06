2ee6a5631db27631ba600cc40361cb3d9ffaa3bd
Theophilus131 <theophilusumar27@gmail.com>
2025-06-06 17:48:48 +0100

    functions set up

---
+++ Python/mestrual_app.py
@@ -0,0 +1,21 @@
+
+from datetime import datetime, timedelta
+
+print("===\n Menstrual App ===\n")
+
+
+def flow_date():
+    next_period_flow_date = first_day_period + cycle_length
+    
+    
+    
+    
+    
+def main():
+    
+    
+    
+    if ___name___ == " __main__":
+        main()
+    
+	
---
+++ Python/test_my_mestrual_app.py
@@ -0,0 +1,9 @@
+
+import mestrual_app
+
+from unittest import TestCase
+
+class TestMestrual(TestCase):
+
+    def test_check_dat_function_exist(self):
+        mestrual_app.flow_date()
