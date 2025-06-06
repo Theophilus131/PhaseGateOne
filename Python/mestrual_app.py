5d796f175b02914342f081fd7c115f7b24ee07ee
Theophilus131 <theophilusumar27@gmail.com>
2025-06-06 22:02:51 +0100

    ovulation and fertile function

--- Python/mestrual_app.py
+++ Python/mestrual_app.py
@@ -1,22 +1,47 @@
 from datetime import datetime, timedelta
 
-print("===\n Menstrual App ===\n")
-
+def get_valid_cycle_length():
+   
+    while True:
+        try:
+            cycle_length = int(input("Enter your average cycle length (21–35 days): "))
+            if 21 <= cycle_length <= 35:
+                return cycle_length
+            else:
+                print("Cycle length must be between 21 and 35 days. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            
 def calculate_next_period(last_period_str, cycle_length):
-    
     last_period_date = datetime.strptime(last_period_str, "%Y-%m-%d")
-    
     next_period_date = last_period_date + timedelta(days=cycle_length)
+    return next_period_date
+
+
+def calculate_ovulation_and_fertile_window(next_period_date):
     
-    return next_period_date.strftime("%Y-%m-%d")
+    ovulation_date = next_period_date - timedelta(days=14)
     
+    fertile_start = ovulation_date - timedelta(days=5)
     
+    fertile_end = ovulation_date + timedelta(days=1)
+    
+    return ovulation_date, fertile_start, fertile_end            
+            
+            
+
+
 
 if __name__ == "__main__":
-   
+    
+    last_period = input("Enter the first day of your last period (YYYY-MM-DD): ")
+    
+    cycle_len = get_valid_cycle_length()
 
-    last_period = input("Enter your last period start date (YYYY-MM-DD): ")
-    cycle_len = int(input("Enter your cycle length (e.g., 28): "))
+    next_period_date = calculate_next_period(last_period, cycle_len)
+    print(" Your next period is expected on:", next_period_date)
 
-    next_period = calculate_next_period(last_period, cycle_len)
-    print("Your next period is expected on:", next_period)
+
+    ovulation, fertile_start, fertile_end = calculate_ovulation_and_fertile_window(next_period_date)
+    print(" Ovulation day:", ovulation.date())
+    print(" Fertile window:", fertile_start.date(), "to", fertile_end.date())
--- Python/test_my_mestrual_app.py
+++ Python/test_my_mestrual_app.py
@@ -4,6 +4,15 @@ import mestrual_app
 from unittest import TestCase
 
 class TestMestrual(TestCase):
+    
+    def test_check_that_function_exist(self):
+        mestrual_app.get_valid_cycle_length()
 
-    def test_check_dat_function_exist(self):
-        mestrual_app.flow_date()
+    def test_check_that_function_exist(self):
+        mestrual_app.calculate_next_period( "2025-05-09", 25)
+        
+        
+    
+       
+        
+       
