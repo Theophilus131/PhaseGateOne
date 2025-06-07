a8db8593e7022c8c9d664e532bc4686c98bf6527
Theophilus131 <theophilusumar27@gmail.com>
2025-06-07 22:06:30 +0100

    progress

--- Java/MenstrualApp.java
+++ Java/MenstrualApp.java
@@ -5,11 +5,18 @@
 	public class MenstrualApp{
 	
 	public static void main(String [] args){
+		Scanner input = new Scanner(System.in);
 	
 	
 	System.out.println("Welcom to Tphils Menstrual App ");
 
-
+	System.out.println("Enter the first day of your last period (YYYY-MM-DD): ");
+	String lastPeriodDate = input.nextLine();
+	
+	LocalDate startDate = LocalDate.parse(lastPeriodDate);
+	
+	System.out.println("Enter your aveage cycle length: ");
+	int cycleLength = input.nextInt();
 }
 
 	}
