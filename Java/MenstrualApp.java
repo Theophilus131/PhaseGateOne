088add9a12ab358f520af5ab9a3a75f98bf368be
Theophilus131 <theophilusumar27@gmail.com>
2025-06-08 00:10:44 +0100

    progress

--- Java/MenstrualApp.java
+++ Java/MenstrualApp.java
@@ -25,7 +25,7 @@
 	
 	
 	System.out.println("\n  Results:");
-	 System.out.println("Next period starts on: " + nextPeriod); 
+	System.out.println("Next period starts on: " + nextPeriod); 
 	System.out.println("Estimated ovulation day: " + ovulationDate);   
 	System.out.println("Fertile window:         " + fertileStart + " to " + fertileEnd);
 	System.out.println("Safe days before:       " + safeBeforeStart + " to " + safeBeforeEnd);
@@ -40,15 +40,33 @@
 	
 	System.out.println("Welcom to Tphils Menstrual App ");
 
+	while(true){
 	System.out.println("Enter the first day of your last period (YYYY-MM-DD): ");
 	String lastPeriodDate = input.nextLine();
 	
+	try {
 	LocalDate startDate = LocalDate.parse(lastPeriodDate);
+	break;
+	} catch (Exception e){
+		System.out.println("invalid date format! enter the date as (YYYY-MM-DD)");
+	}
+	}
 	
+	
+	int cycleLength = 0;
+	while(true){
 	System.out.println("Enter your aveage cycle length: ");
-	int cycleLength = input.nextInt();
+	cycleLength = input.nextInt();
 	
-	 calculateCycle(startDate, cycleLength);
+	if (cycleLength >= 21 && cycleLength <= 35) {
+    break;
+	} else {
+    System.out.println("Invalid cycle length. Please enter a value between 21 and 35 days.");
+	}
+	
+}
+	
+	calculateCycle(startDate, cycleLength);
 	
 
 	}
