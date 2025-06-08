7aa0f67eaf632465487bac62872d8af53d58b184
Theophilus131 <theophilusumar27@gmail.com>
2025-06-08 18:22:52 +0100

    testing

--- Java/EcommerceCheckout.java
+++ Java/EcommerceCheckout.java
@@ -14,24 +14,26 @@
 	System.out.println("What did customer buy: ");
 	String productName = input.nextLine();
 
-	System.out.println("How many Quality of product: ");
+	System.out.println("How many Quantity of the product did customer buy: ");
 	double quality = input.nextDouble();
 
 	System.out.println("price par unit: ");
 	double price = input.nextDouble();
 	
-	double VAT = 7.5 / 100;
+	}
+public static void header(){
+System.out.print("SEMICOLON STORES");
+System.out.print("MAIN BRANCH");
+System.out.print("LOCATION: 312, HEBERT MACAULY WAY, SABO YABA, LAGOS.");
+System.out.print("TEL: 03293828343");
+System.out.print("Date: 09 - ");
+System.out.print("Cashier: Cashier's Name ");
+System.out.print("Customer Name: ");
 
-	double totalPrice = (quality * price) + VAT;
-
-	System.out.println("Amount to be paid :" + totalPrice );
 	
 
 
-	
-
-
-
+header();
 
 
 
---
+++ Java/TestMenstrualApp.java
@@ -0,0 +1,17 @@
+
+	 import org.junit.jupiter.api.Test;
+	 import java.time.LocalDate;
+	 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+	public class TestMenstrualApp{
+	
+	@Test
+	public void testThatFunctionExist(){
+	MenstrualApp.calculateCycle(LocalDate.of(2025, 6, 6), 28);
+
+
+	}
+
+	
+	
+}
