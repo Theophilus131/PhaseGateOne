a2d12c99239f527db7bfa011d613da9b4759578f
Theophilus131 <theophilusumar27@gmail.com>
2025-06-10 23:36:14 +0100

    card validation done

--- Python/card_validation.py
+++ Python/card_validation.py
@@ -5,22 +5,37 @@ card_number =  input("kindly enter your card details for verification :")
 def validating_card_detail(card_number):
     
     card_length = len(card_number)
-    #sum_even = 0
-    #sum_odd = 0
-    
-    #for count in card_number:
-        #if card_number[count]
+   
 	
-    if card_length == 16 and card_number.startswith("4"):
+    if card_length == 16 and card_number[0] == "4":
         return "Visa"
-    elif card_length == 16 and card_number("5"):
+    elif card_length == 16 and card_number[0]== "5":
         return "master_card"
-    elif card_length == 16 and card_number(6):
+    elif card_length == 16 and card_number[0] == "6":
         return "Discover"
-    elif card_length == 15 and card_number(3):
+    elif card_length == 15 and card_number[:2]== "37":
         return "America Express"
     else:
         return "invalid card type"
-            
+        
+    total = 0
+    double = False
+    
+    for count in range(card_length -1, -1, -1):
+        digit = int(card_number(count))
+        
+    if double:
+        digit *= 2
+    if digit > 9:
+        digit -= 9
+        
+    total += digit
+    double = not double
+    
+    if total % 10 == 0:
+        return f" Valid {card_type} card"
+    else:
+        
+        return f" Invalid {card_type} card "    
         
 print(validating_card_detail(card_number))
