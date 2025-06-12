3f4993914428a26b8348b4fe8303eee3315e5cb1
Theophilus131 <theophilusumar27@gmail.com>
2025-06-12 09:50:56 +0100

    validating testing

---
+++ Java/ValidateCardTest.java
@@ -0,0 +1,26 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ValidateCardTest {
+	
+	@Test
+	public void testShortInput() {
+    String result = ValidateCard.validatingCardDetail("123");
+    assertEquals("invalid card type", result);
+}
+
+	
+    @Test
+    public void testValidVisaCard() {
+
+	String input = ValidateCard.validatingCardDetail("4111111111111111");
+	String expected = "Visa";
+	
+	assertEquals("Visa", input);
+	
+	
+	}
+	
+	
+}
