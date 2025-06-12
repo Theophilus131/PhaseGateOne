02d33403c867bb93d6bfe8537b4f2531f30b9355
Theophilus131 <theophilusumar27@gmail.com>
2025-06-12 11:40:04 +0100

    visa testing

--- Python/test_validation_card.py
+++ Python/test_validation_card.py
@@ -1,14 +1,18 @@
 
-
 import card_validation
 
 from unittest import TestCase
 
 class TestCardValidation(TestCase):
 
-	def test_check_short_input(self):
-	
-		result = card_validation.validating_card_detail("123")
-		expected = "invalid card type"
-		self.assertEqual(result, expected)
+    def test_check_short_input(self):
+        result = card_validation.validating_card_detail("123")
+        expected = "invalid card type"
+        self.assertEqual(result, expected)
+        
+    def test_check_visa_card_is_valid(self):
+        result = card_validation.validating_card_detail("4111111111111111")
+        expected = "Visa"
+        self.assertEqual(result, expected)
+        
     
