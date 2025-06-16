a40edf9fcc0e6e8c3eaaaddf2d04be80c49a5991
Theophilus131 <theophilusumar27@gmail.com>
2025-06-16 15:51:26 +0100

    working on student grade

---
+++ OldSnacks/student_grade.py
@@ -0,0 +1,40 @@
+
+
+
+number_student = int(input("How many students do you have?: "))
+number_subject = int(input("How many subjects do they offer?: "))
+
+for student in range(1, number_student + 1):
+    print(f"\nEntering scores for Student {student}")
+    
+    for subject in range(1, number_subject + 1):
+        score = int(input(f"  Enter score for Subject {subject}: "))
+       
+
+
+    
+ 
+    """
+    
+    score += 1
+if score < 0 and score <= 100:
+        print("invalid score")
+        
+
+print("saving >>>>>>>>>>>>>>>>>>>>>>>")
+print("Saved successfully")
+
+
+total = 0
+average = 0
+
+#for number in number_student and number_subject:
+
+def calculating_total():
+	total += score
+
+def calculating_average():
+
+	average = total/ number_subject
+    
+    """
