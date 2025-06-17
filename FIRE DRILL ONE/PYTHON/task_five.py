af6cdedb000cf600ad015a8ebeab9ca2b2abf4b7
Theophilus131 <theophilusumar27@gmail.com>
2025-06-17 17:17:39 +0100

    python fire drill

---
+++ FIRE DRILL ONE/PYTHON/task_five.py
@@ -0,0 +1,20 @@
+for number in range(1, 11):
+	if(number % 4 == 0):
+		print(number)
+
+	if(number % 4 == 0):
+		print(number)
+
+	if(number % 4 == 0):
+		print(number)
+
+	if(number % 4 == 0):
+		print(number)
+
+	if(number % 4 == 0):
+		print(number)
+
+
+
+
+	
---
+++ FIRE DRILL ONE/PYTHON/task_six.py
@@ -0,0 +1,9 @@
+for number in range(1, 6):
+	print(4 ** number)
+
+for index in range(1, 6):
+	print(8 ** index)
+
+
+
+
